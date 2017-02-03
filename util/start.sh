5675dd4f5cd5733dc286885d86e5d2b0ccf461af
David R. Andersen <k0rx@RXcomm.net>
2017-02-03 14:08:57 -0600

    Clean up afterward
    This commit adds the --rm flag to the docker run command.
    As a result, the container will be purged after running.
    
    Note that any changes made to the host's ~/.config/unMessage
    will persist.

--- util/start.sh
+++ util/start.sh
@@ -3,11 +3,11 @@
 RUNMODE=$1
 
 test "$RUNMODE" == "gui" && \
-  sudo docker run -it \
+  sudo docker run -it --rm \
     -e DISPLAY=$DISPLAY \
     -e LOCAL_USER_ID=`id -u $USER` \
     -v /home/$USER/.config:/home/user/.config \
     -v /tmp/.X11-unix:/tmp/.X11-unix rxcomm/unmessage-client || \
-  sudo docker run -it \
+  sudo docker run -it --rm \
     -e LOCAL_USER_ID=`id -u $USER` \
     -v /home/$USER/.config:/home/user/.config rxcomm/unmessage-client
